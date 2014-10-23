148adcc9cc78f3b8ffa7e621b4bd6fa69b5dea69
William Huang <william.w.huang@gmail.com>
2014-10-23 16:56:22 -0700

    part 4

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -7,4 +7,18 @@ class PokemonController < ApplicationController
     redirect_to root_path
   end
 
+  def damage
+    currPoke = Pokemon.find(params[:id])
+    currPoke.health = currPoke.health - 10
+    currPoke.save
+    redirect_to trainer_path(currPoke.trainer_id)
+  end
+
+  def heal
+    currPoke = Pokemon.find(params[:id])
+    currPoke.health = currPoke.health + 10
+    currPoke.save
+    redirect_to trainer_path(currPoke.trainer_id)
+  end
+
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -8,6 +8,7 @@ class TrainersController < ApplicationController
   def show
     @trainer = Trainer.find(params[:id])
     @pokemon = Pokemon.where(trainer_id: @trainer)
+    @heal_pokemon = Pokemon.where("trainer_id = ? AND health < ?", @trainer, 100)
   end
 
 end
